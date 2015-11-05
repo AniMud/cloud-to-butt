8c1d7026df380544c43f49d3a790d484a3b1c3b7
Velkyr <AniMud@users.noreply.github.com>
2015-11-05 09:44:25 -0400

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,12 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bStephen Harper\b/g, "Calgary International Airport");
+	v = v.replace(/\bStephen harper\b/g, "Calgary International Airport");
+	v = v.replace(/\bstephen Harper\b/g, "Calgary International Airport");
+	v = v.replace(/\bstephen harper\b/g, "Calgary International Airport");
+	v = v.replace(/\bHarper\b/g, "Calgary International Airport");
+	v = v.replace(/\bharper\b/g, "Calgary International Airport");
 	
 	textNode.nodeValue = v;
 }
